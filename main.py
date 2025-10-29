356fee6c1b34796ff6a1794ffcc4199c0c29806e
Avtandil Pangani <140119617+avtotrainer@users.noreply.github.com>
2025-10-29 05:29:26 +0400

    Change BOT_TOKEN to new value
    Updated the BOT_TOKEN to a new value.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-BOT_TOKEN = "YOUR_BOT_TOKEN_HERE"
+BOT_TOKEN = "6949273407:AAF242vDELBssmQjm_oooxe6jkafmk7Y-YQ"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("გამარჯობა! 🖐")
